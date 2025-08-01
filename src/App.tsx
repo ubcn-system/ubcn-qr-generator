7a8259a40bb0dd0b18645a2d3e6f4aab44662917
JinSoo <pjinsoo0421@gmail.com>
2025-08-01 13:47:21 +0900

    chore($route): fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CommonPage from "./pages/CommonPage";
 import WelpointPage from "./pages/WelpointPage";
 import ErrorPage404 from "./pages/ErrorPage404.tsx";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/common" element={<CommonPage />} />
                 <Route path="/welpoint" element={<WelpointPage />} />
                 <Route path="*" element={<ErrorPage404 />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
