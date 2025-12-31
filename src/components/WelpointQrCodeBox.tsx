7ea4972499ca54fd6f314081a58416b035df523e
JinSoo <pjinsoo0421@gmail.com>
2025-12-31 15:51:01 +0900

    design($text): modify text
    안내 문구 수정

--- src/components/WelpointQrCodeBox.tsx
+++ src/components/WelpointQrCodeBox.tsx
@@ -24,10 +24,12 @@ function WelpointQrCodeBox({ cardNumber, expiredDate }: WelpointQrCodeBoxProps) 
     return (
         <div className="flex flex-col items-center mt-20">
             <CalloutsBlock>
-                <p>✅ 위 페이지는 <strong>카카오 알림톡 발송일자로부터 3일까지만</strong> 접속 가능합니다.</p>
-                <p>✅ QR코드는 다운로드 후 <strong>타인과 공유하지 마시고 안전하게 보관</strong>해 주십시오.</p>
+                <p>✅ 이 페이지는 <strong>카카오 알림톡을 받은 날로부터 3일 동일만</strong> 확인할 수 있습니다.</p>
+                <p>✅ 아래의 <strong className={"text-blue-500"}>QR코드 다운로드</strong> 버튼을 통해 <strong>QR코드를 평생 보존</strong>하실 수 있습니다.</p>
+                <p>✅ 다운로드 된 QR코드는 <strong>3일후에도 사용 가능</strong>하며 갤러리나 사진첩에서 확인 가능합니다.</p>
+                <br/>
                 <p>⚠️ QR코드의 <strong>관리에 대한 책임은 개인에게</strong> 있습니다.</p>
-                <p>⚠️ QR코드의 <strong>분실·도용 등 문제에 대해서 유비씨엔은 어떠한 책임도 지지 않습니다.</strong></p>
+                <p>⚠️ QR코드의 <strong>분실·도용 등 문제에 대해서 당사는 어떠한 책임도 지지 않습니다.</strong></p>
             </CalloutsBlock>
             <div className={"mb-2 text-sm text-gray-500 flex flex-col items-center"}>
                 <p>접속 만료 예정 일자 </p>
